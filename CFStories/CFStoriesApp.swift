40825672437354874b210c3da9a96984060d9ef5
Farhan Farooqui <farhan@conchalabs.com>
2020-07-18 18:03:28 -0700

    Initial Commit

---
+++ CFStories/CFStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CFStoriesApp.swift
+//  CFStories
+//
+//  Created by Farhan Farooqui on 7/18/20.
+//
+
+import SwiftUI
+
+@main
+struct CFStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
